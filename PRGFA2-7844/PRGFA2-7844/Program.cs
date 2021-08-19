c60fcd00df48df5f9738294fb2cd827ec10cb0bc
7844CJ <7844@ctucareer.co.za>
2021-08-19 18:58:56 +0200

    First Commit of Console Application

---
+++ PRGFA2-7844/PRGFA2-7844/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PRGFA2_7844
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
