65c583fce1e6c4de071a1c03cbd8c55eac9df109
7844CJ <7844@ctucareer.co.za>
2021-08-19 19:32:56 +0200

    Creating Pull Request

--- PRGFA2-7844/PRGFA2-7844/Program.cs
+++ PRGFA2-7844/PRGFA2-7844/Program.cs
@@ -7,6 +7,7 @@ namespace PRGFA2_7844
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("How to do a Pull Request");
         }
     }
 }
